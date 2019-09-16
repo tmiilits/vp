d14cb14afbeef3bf47eaecab13527fa2eff01910
Tauri Miilits <tmiilits@users.noreply.github.com>
2019-09-16 11:43:35 +0300

    kolmas tund

---
+++ 03 tund/try.php
@@ -0,0 +1,39 @@
+<?php
+$userName = "Tauri Miilits";
+$fullTimeNow = date("d.m.Y H:i:s");
+$hourNow = date("H");
+$partOfDay = "hägune aeg";
+if($hourNow < 8) {
+$partOfDay = "varane hommik";	
+}
+?>
+
+<!DOCTYPE html>
+<html lang="et">
+<head>
+<meta charset="utf-8">
+  <title>Tauri progeb</title>
+  <?php
+  echo $userName;
+  ?>
+<head/>
+<body>
+<?php
+echo "<h1>" .$username ."koolitöö leht </h1>";
+?>
+  <h1>Tauri koolitöö leht</h1>
+  <p>See leht on loodud koolis õppetöö raames ja ei sisalda tõsiseltvõetavat sisu!</p>
+  <hr>
+  <p> lehe avamise hetkel oli aeg:
+  <?php
+  echo $fullTimeNow;
+  ?>
+  .<p>
+  <?php
+  echo "<p>Lehe avamise hetkel oli " .$partOfDay .
+  ".</p>";
+  ?>
+  <hr>
+</body)
+
+</html>
